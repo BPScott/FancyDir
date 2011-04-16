2cff5d82c6aa60fc1b0556a2571e61564cce22d5
BPScott <ben@reload.me.uk>
2011-04-16 18:49:01 +0100

    Make sure we pass JSHint, use more native JS where useful

--- fancydir/javascript.js
+++ fancydir/javascript.js
@@ -1,56 +1,54 @@
-$(document).ready(function() {
+$(function() {
+//Get the filename elements now rather than on every keyup
+var filenameeles = $('table tr:gt(1) td:nth-child(2) a');
 
-//Striped Rows
+//Striped rows
 $('table tr:gt(0):odd').addClass('alt');
 
-//append search box to the dom
+//Search box
 $('<input type="text" id="filter" title="Search"/>').blur(function (){
-	if (this.value == '') { $(this).val(this.title).addClass('hint'); }
+	if (this.value === '') { $(this).val(this.title).addClass('hint'); }
 }).focus(function (){ 
-	if (this.value == this.title) { $(this).val('').removeClass('hint'); } 
-}).blur().insertBefore('h1');
-
-//Get the filename elements now rather than on every keyup
-filenameeles = $('table tr:gt(1) td:nth-child(2) a');
-
-//Search filter
-$('#filter').keyup(function(e) {
-	var filterval = $('#filter').val();
-	
+	if (this.value === this.title) { $(this).val('').removeClass('hint'); } 
+}).keyup(function(e){
 	//short circuit empty search box	
-	if (filterval == '') {
+	if (this.value === '') {
 		//force everything to show and reset striped rows
 		$('table tr:gt(0)').each(function(i){
-			this.style['display'] = 'table-row';
+			this.style.display = 'table-row';
 			this.className = ((i % 2) ? 'alt' : '');
 		});
 	}
 	else {
-		var filterregex = new RegExp(filterval, "i"), i=0;
+		var filterregex = new RegExp(this.value, "i"), i=0;
 		filenameeles.each(function() {
 			//parentNode.parentNode shall be the tr
 			if (filterregex.test(this.innerHTML)){
-				this.parentNode.parentNode.style['display'] = 'table-row';
+				this.parentNode.parentNode.style.display = 'table-row';
 				//Work out the striped rows as we go
 				this.parentNode.parentNode.className = ((++i % 2)? 'alt' : '');
 			}
 			else {
-				this.parentNode.parentNode.style['display'] = 'none';
-			};
+				this.parentNode.parentNode.style.display = 'none';
+			}
 		});
-	};
-});
+	}
+}).blur().insertBefore('h1');
+
 
 //Turn directory name into breadcrumb links
-breadcrumbs = $('h1').text().match(/[^:]*: (.*)\//)[1].split('/');
-pathsofar = document.location.protocol + '//';
-$.each(breadcrumbs, function(i) {
-	pathsofar = pathsofar + this + '/';
-	breadcrumbs[i] = ('<a href="' + pathsofar + '">' + decodeURI(this) + '</a>');
-});
-$('h1').html($('h1').text().replace(/: (.*)\//, ': ' + breadcrumbs.join('/') + '/'));
+var heading = document.getElementsByTagName('h1')[0];
+var breadcrumbs = heading.innerHTML.match(/[^:]*: (.*)\//)[1].split('/');
+var pathsofar = document.location.protocol + '//';
 
-//fix for IE 6not knowing of some CSS selectors  - Yes I know browser sniffing is bad and this shouldn't make a difference to other browsers, but you can't be too careful
+for (var i in breadcrumbs) {
+	pathsofar += breadcrumbs[i] + '/';
+	breadcrumbs[i] = ('<a href="' + pathsofar + '">' + decodeURI(breadcrumbs[i]) + '</a>');
+}
+heading.innerHTML = heading.innerHTML.replace(/: (.*)\//, ': ' + breadcrumbs.join('/') + '/');
+
+
+//fix for IE 6 not knowing of some CSS selectors  - Yes I know browser sniffing is bad and this shouldn't make a difference to other browsers, but you can't be too careful
 if (jQuery.browser.msie && jQuery.browser.version == '6.0') {
 	$('table tr td:first-child').addClass('IE_fixleftborder');
 	$('table td+td+td+td').addClass('IE_fixrightborder');
@@ -62,6 +60,6 @@ if (jQuery.browser.msie && jQuery.browser.version == '6.0') {
 	$('td+td+td').addClass('IE_fixcol3');
 	$('td+td+td+td').addClass('IE_fixcol4');
 	$('td+td+td+td+td').addClass('IE_fixcol5');
-};
+}
 
 });
